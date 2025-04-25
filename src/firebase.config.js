c7b609294c7535e582d7fd64e92bd0be58289c04
sudeep-kumar0 <sudeepkumarukh@gmail.com>
2025-04-26 01:26:31 +0530

    Added environment variables for Firebase and Cloudinary

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -1,17 +1,16 @@
-// src/firebase.config.js
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
+// Use environment variables for Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBWkeI-R8QveLFI3dxZvkfVgKPIrdcNqSg",
-  authDomain: "real-92ba5.firebaseapp.com",
-  databaseURL:
-    "https://real-92ba5-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "real-92ba5",
-  storageBucket: "real-92ba5.firebasestorage.app",
-  messagingSenderId: "735002193972",
-  appId: "1:735002193972:web:ffa9e064c13caa69ba1a61",
-  measurementId: "G-2WXB52JDLR",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
--- src/pages/cloudinary.js
+++ src/pages/cloudinary.js
@@ -1,13 +1,12 @@
-// src/utils/cloudinary.js
 import axios from "axios";
 
 export const cloudinaryUpload = async (file) => {
-  const url = "https://api.cloudinary.com/v1_1/dmu7j8wov/image/upload"; // replace with your Cloudinary Cloud Name
-  const uploadPreset = "ml_default"; // replace with your upload preset
+  const url = process.env.REACT_APP_CLOUDINARY_URL; // Use Cloudinary URL from environment variable
+  const uploadPreset = process.env.REACT_APP_CLOUDINARY_UPLOAD_PRESET; // Use Upload Preset from environment variable
 
   const formData = new FormData();
   formData.append("file", file);
-  formData.append("OdZJDM2mJ_rQak89yH9zTrKoLik", uploadPreset);
+  formData.append("upload_preset", uploadPreset); // Fix key here
 
   try {
     const response = await axios.post(url, formData);
